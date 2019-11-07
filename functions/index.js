f52de08ab000fefcbe6fe6be08671f3973d9469c
ktanakap <ktanakap@wellesley.edu>
2019-11-07 15:05:25 -0500

    connecting users to database

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,18 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+// Add a user to Firestore database:
+exports.setupUsers = functions.auth.user().onCreate(async (user) => {
+  var fullName = user.displayName || 'Anonymous';
+  //var photo = user.photoURL || 'None';
+  await admin.firestore().collection('users').doc(user.uid).set({
+    username: fullName,
+    user_id: user.uid
+    //profile_pic: photo
+  });
+});
+
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
