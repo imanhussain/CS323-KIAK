97d419b1189678d4d92789a9ffc399868b625d88
imanhussain <ihussain@wellesley.edu>
2019-11-10 15:47:36 -0500

    p3 progress

--- functions/index.js
+++ functions/index.js
@@ -5,13 +5,25 @@ admin.initializeApp();
 // Add a user to Firestore database:
 exports.setupUsers = functions.auth.user().onCreate(async (user) => {
   var fullName = user.displayName || 'Anonymous';
-  //var photo = user.photoURL || 'None';
+  var photo = user.photoURL || 'None';
   await admin.firestore().collection('users').doc(user.uid).set({
     username: fullName,
-    user_id: user.uid
-    //profile_pic: photo
+    user_id: user.uid,
+    profession: "Unspecified",
+    connection_count: 0,
+    profile_pic: photo,
+    date_joined: user.metaData.creationDate,
+    
+/*
+    profile_pic: photo,
+    date_joined: user.metaData.creationDate*/
   });
+/*    await admin.firestore().collection('users').doc(user.uid).collection("user_connections").doc("info").set({
+        connection_count: 0,
+    });  */
 });
+
+
 
 
 // // Create and Deploy Your First Cloud Functions
